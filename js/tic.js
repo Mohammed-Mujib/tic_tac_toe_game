798bb087dfdab0ca74f1c18a7d2fe1a9953abd4b
Mohammed-Mujib <wadmujib@gmail.com>
2024-04-06 03:11:36 +0200

    fix ligical

--- js/tic.js
+++ js/tic.js
@@ -1,46 +1,87 @@
-let element = document.querySelectorAll(".mincon");
-let curent = "x"
-let look = [ , , ,
-             , , ,
-             , , 
-            ];
-let row1 = 0;
-let row2 = 0;
-let row3 = 0;
-document.body.onclick = function(e){
-    if(e.target.classList.contains("box")){
-        if (e.target.textContent==="") {
-            e.target.textContent = curent;
-            if(curent==="x"){
-                e.target.style.color = "black";
-                look[e.target.dataset.number]="x";
-                curent="o";
-                }
-            else{
-                e.target.style.color = "black";
-                look[e.target.dataset.number]="o";
-                curent="x";
-                }
-                
-            }
-            
-            // for(let i = 0;i<9;i++){
-            //     if(i==0){
-            //         if 
-            //     }
-            
-            // }
-            
-        }
+const X_CLASS = "x";
+const CIRCEL_CLASS = "circel";
+const board = document.getElementById('board');
+const winningMessageElement = document.getElementById("winning-message");
+const winningMessageTextElement= document.querySelector("[data-winning-message-text]");
+const winning_compo = [
+                    [0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],
+                    [2,4,6]
+                            ];
+const cellElements = document.querySelectorAll('[data-cell]');
+let circelTurn
+const resturtButton = document.getElementById("restart");
+startGame();
+resturtButton.addEventListener("click",startGame)
+function startGame(){
+    circelTurn = false
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCEL_CLASS)
+        cell.removeEventListener("click",handleClick)
+        cell.addEventListener('click', handleClick, { once: true })
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove("show");
+}
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circelTurn ? CIRCEL_CLASS : X_CLASS
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)){
+        endGame(false)
+    } else if(isDraw()){
+        endGame(true)
+    }else{
+        swapTurns();
+        setBoardHoverClass();
     }
-// document.body.addEventListener( "mouseenter",function(e){
-//     if(e.target.classList.contains("box")){
-//     if (e.target.textContent.length>0) {
-//         e.target.style.cursor = "auto";
-//         console.log(e.target.textContent.length);
-//     }
-//     else{
-//         e.target.style.cursor = "pointer";
-//     }
-// }
-// },false)
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.textContent = "Draw";
+    }
+    else{
+        winningMessageTextElement.textContent = `${circelTurn ? "O's":"X's"} wins`;
+        }
+    winningMessageElement.classList.add("show");
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCEL_CLASS)
+    })
+}
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circelTurn = !circelTurn
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCEL_CLASS)
+    if (circelTurn) {
+        board.classList.add(CIRCEL_CLASS)
+    }
+    else{
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass){
+    return winning_compo.some(copo =>{
+        return copo.every(index =>{
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
